18e6afb7cdb1560ab2f2a42bd9355668fcd1f3a8
Rajeesh <noreply>
2017-03-17 05:28:15 +0000

    Add module Login

---
+++ login.js
@@ -0,0 +1,15 @@
+/**
+ * @module //login
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Login
+ * @extends Montage
+ */
+exports.Login = Montage.specialize(/** @lends Login# */ {
+    constructor: {
+        value: function Login() {
+            this.super();
+        }
+    }
+});
